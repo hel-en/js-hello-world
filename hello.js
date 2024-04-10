6afec1f7d2b60e70b065597cc7be20def6ada473
User <helen@MacBook-Pro-53712.local>
2024-04-10 17:43:37 -0500

    Now says Hello, World

--- hello.js
+++ hello.js
@@ -4,19 +4,34 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello, World!")
+
+// Exercises
+console.log('Hello, World!') // 1
+// 2 parentheses are necessary
+// 3 same function for both types, both opening/closing quotation marks necessary; must match
+// 4 semicolon not always necessary; works for printing code to next line if typed on same line; funtions like pressing "Enter"
+console.log(8); console.log(8+8) // 5
+console.log("What's up?") // 6
+console.log("Nothing much.") // 6
+console.log("What's up?"); console.log("Nothing much.") // 6
+console.log("What's up?", 
+            "Nothing much."); // 7 separate same line texts with commas
+console.log("He said, 'What's up?' and she replied, 'Nothing much.'") // 8
+console.log('He said, "What is up?" and she replied, "Nothing much."') // 8
+console.log((2**3)*(2)); // 9
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
+   1. Change the message that is printed. 
    2. Figure out what the parentheses do. Will the code work without them?
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
+   4. Remove the semi-colon, ;. #not necessary
    5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+   6. Print multiple messages one after the other. 
+   7. Print two messages on the same line. 
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore". 
    9. Other. You choose!
 */
